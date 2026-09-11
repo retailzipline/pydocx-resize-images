89cdb4224ace0af72fbc515019dab09a36ff87a7
Andy Kopp <andy@retailzipline.com>
2026-09-11 12:38:19 -0500

    bumps and fixture fixes

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ def main():
         scripts=[],
         zip_safe=False,
         install_requires=[
-            'requests>=2.31.0',
-            'Pillow==10.2.0'
+            'requests>=2.34.2',
+            'Pillow==12.3.0'
         ],
         cmdclass={},
         classifiers=[
